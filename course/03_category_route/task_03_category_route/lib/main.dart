298148d599630eb34183ae244c916a0741fad5d1
wakamezake <hotwater1367@gmail.com>
2020-01-03 20:44:36 +0900

    init

--- course/03_category_route/task_03_category_route/lib/main.dart
+++ course/03_category_route/task_03_category_route/lib/main.dart
@@ -6,6 +6,9 @@
 import 'package:flutter/material.dart';
 
 // TODO: Import the CategoryRoute widget
+import 'package:task_03_category_route/category_route.dart';
+
+import 'category_route.dart';
 
 /// The function that is called when main.dart is run.
 void main() {
@@ -23,7 +26,7 @@ class UnitConverterApp extends StatelessWidget {
       title: 'Unit Converter',
       // TODO: Instead of pointing to exactly 1 Category widget,
       // our home should now point to an instance of the CategoryRoute widget.
-      home: Container(),
+      home: CategoryRoute(),
     );
   }
 }
