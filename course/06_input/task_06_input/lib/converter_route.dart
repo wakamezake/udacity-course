21ed909a78db2bd1fc3ee8ca0b3994933273d44c
wakamezake <hotwater1367@gmail.com>
2020-01-04 18:05:22 +0900

    init

--- course/06_input/task_06_input/lib/converter_route.dart
+++ course/06_input/task_06_input/lib/converter_route.dart
@@ -35,8 +35,45 @@ class ConverterRoute extends StatefulWidget {
 class _ConverterRouteState extends State<ConverterRoute> {
   // TODO: Set some variables, such as for keeping track of the user's input
   // value and units
+  Unit _fromValue;
+  Unit _toValue;
+  double _inputValue;
+  String _convertedValue = '';
+  List<DropdownMenuItem> _unitMenuItems;
+  bool _showValidationError = false;
 
   // TODO: Determine whether you need to override anything, such as initState()
+  @override
+  void initState() {
+    super.initState();
+    _createDropdownMenuItems();
+    _setDefaults();
+  }
+
+  void _createDropdownMenuItems() {
+    var newItems = <DropdownMenuItem>[];
+    for (var unit in widget.units) {
+      newItems.add(DropdownMenuItem(
+        value: unit.name,
+        child: Container(
+          child: Text(
+            unit.name,
+            softWrap: true,
+          ),
+        ),
+      ));
+    }
+    setState(() {
+      _unitMenuItems = newItems;
+    });
+  }
+
+  void _setDefaults() {
+    setState(() {
+      _fromValue = widget.units[0];
+      _toValue = widget.units[1];
+    });
+  }
 
   // TODO: Add other helper functions. We've given you one, _format()
 
@@ -56,41 +93,152 @@ class _ConverterRouteState extends State<ConverterRoute> {
     return outputNum;
   }
 
+  void _updateInputValue(String input) {
+    setState(() {
+      if (input == null || input.isEmpty) {
+        _convertedValue = '';
+      } else {
+        try {
+          final inputDouble = double.parse(input);
+          _showValidationError = false;
+          _inputValue = inputDouble;
+          _updateCoversion();
+        } on Exception catch (e) {
+          print('Error: $e');
+          _showValidationError = true;
+        }
+      }
+    });
+  }
+
+  Unit _getUnit(String unitName) {
+    return widget.units.firstWhere(
+      (Unit unit) {
+        return unit.name == unitName;
+      },
+      orElse: null,
+    );
+  }
+
+  void _updateCoversion() {
+    setState(() {
+      _convertedValue =
+          _format(_inputValue * (_toValue.conversion / _fromValue.conversion));
+    });
+  }
+
+  void _updateFromConversion(dynamic unitName) {
+    setState(() {
+      _fromValue = _getUnit(unitName);
+    });
+    if (_inputValue != null) {
+      _updateCoversion();
+    }
+  }
+
+  void _updateToConversion(dynamic unitName) {
+    setState(() {
+      _toValue = _getUnit(unitName);
+    });
+    if (_inputValue != null) {
+      _updateCoversion();
+    }
+  }
+
+  Widget _createDropdown(String currentValue, ValueChanged<dynamic> onChanged) {
+    return Container(
+      margin: EdgeInsets.only(top: 16.0),
+      decoration: BoxDecoration(
+          color: Colors.grey[50],
+          border: Border.all(
+            color: Colors.grey[400],
+            width: 1.0,
+          )),
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.grey[50],
+        ),
+        child: DropdownButtonHideUnderline(
+          child: ButtonTheme(
+            alignedDropdown: true,
+            child: DropdownButton(
+              value: currentValue,
+              items: _unitMenuItems,
+              onChanged: onChanged,
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: Create the 'input' group of widgets. This is a Column that
     // includes the input value, and 'from' unit [Dropdown].
-
+    final input = Padding(
+      padding: _padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextField(
+            style: Theme.of(context).textTheme.display1,
+            decoration: InputDecoration(
+                labelStyle: Theme.of(context).textTheme.display1,
+                errorText: _showValidationError ? 'Invalid number' : null,
+                labelText: 'Input'),
+            keyboardType: TextInputType.number,
+            onChanged: _updateInputValue,
+          ),
+          _createDropdown(_fromValue.name, _updateFromConversion),
+        ],
+      ),
+    );
     // TODO: Create a compare arrows icon.
 
+    final arrows = RotatedBox(
+      quarterTurns: 1,
+      child: Icon(
+        Icons.compare_arrows,
+        size: 40.0,
+      ),
+    );
     // TODO: Create the 'output' group of widgets. This is a Column that
     // includes the output value, and 'to' unit [Dropdown].
-
+    final output = Padding(
+      padding: _padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          InputDecorator(
+            child: Text(
+              _convertedValue,
+              style: Theme.of(context).textTheme.display1,
+            ),
+            decoration: InputDecoration(
+              labelText: 'Output',
+              labelStyle: Theme.of(context).textTheme.display1,
+            ),
+          ),
+          _createDropdown(_toValue.name, _updateToConversion),
+        ],
+      ),
+    );
     // TODO: Return the input, arrows, and output widgets, wrapped in a Column.
+    final converter = Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        input,
+        arrows,
+        output,
+      ],
+    );
 
-    // TODO: Delete the below placeholder code.
-    final unitWidgets = widget.units.map((Unit unit) {
-      return Container(
-        color: widget.color,
-        margin: EdgeInsets.all(8.0),
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              unit.name,
-              style: Theme.of(context).textTheme.headline,
-            ),
-            Text(
-              'Conversion: ${unit.conversion}',
-              style: Theme.of(context).textTheme.subhead,
-            ),
-          ],
-        ),
-      );
-    }).toList();
-
-    return ListView(
-      children: unitWidgets,
+    return Padding(
+      padding: _padding,
+      child: converter,
     );
   }
 }
